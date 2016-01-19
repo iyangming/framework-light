891b3450b8dd8774bf4dcf7b0909c8fe25c0a733
Abel <sdwyub@qq.com>
2016-01-19 08:50:35 +0800

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name liveApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the liveApp
+ */
+angular.module('liveApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name liveApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the liveApp
+ */
+angular.module('liveApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
